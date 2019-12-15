4e96c8a5f7e19c1e3a58548d7cb6ee9ef7813080
moriokashingo <uma20056@yahoo.co.jp>
2019-12-16 01:10:00 +0900

    Create index.blade.php

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('title',"WTM~ What's The Music?~")
+
+@section('content')
+  <h1>ここから</h1>
+    @foreach($questions as $question)
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">
+          <a href="{{route('questions.show',$question->id)}}">
+          {{$question->description}}
+          </a>
+        </h5>
+        <h5 class="card-title">
+          投稿者:
+          <a href="#">
+            {{$question->user->name}}
+          </a>
+        </h5>
+        <div class="resolution">
+          @if($question->resolution=="0")
+            <div class="resolution__still">
+            未解決
+            </div>
+          @else
+            <div class="resolution__already">
+            解決済み
+            </div>
+          @endif
+        </div>
+        @if(Auth::id() == $question->user_id)
+          <a href="{{route('questions.edit',$question->id)}}" class ="btn btn-primary" >
+          編集する
+          </a>
+        @endif
+
+      </div>
+    </div>
+    @endforeach
+    @if(isset($tag_id))
+        {{$posts->appends(['category_id'=> $category_id])->links()}}
+    @elseif(isset($search_query))
+        {{$posts->appends(['search_query'=> $search_query])->links()}}
+    @else
+        {{$questions->links()}}
+    @endif
+@endsection
