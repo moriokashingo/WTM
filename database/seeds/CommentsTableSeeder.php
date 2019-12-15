1dc89b20c576d4667cf5b65c30beeb4c624e70fe
moriokashingo <uma20056@yahoo.co.jp>
2019-12-16 01:10:54 +0900

    Create CommentsTableSeeder.php

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('comments')->insert([
+            ['body' => 'book',
+            'question_id'=>'1',
+            'user_id'=>'1'
+            ],
+            ['body' => 'cafe',
+            'question_id'=>'1',
+            'user_id'=>'1'],
+            ['body' => 'travel',
+            'question_id'=>'1',
+            'user_id'=>'1']
+        ]);
+    }
+}
