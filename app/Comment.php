8f9f90dd7dcb604ad5a9f418280ec444629b6cf8
moriokashingo <uma20056@yahoo.co.jp>
2019-12-18 01:07:51 +0900

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    protected $fillable=['body',"user_id","question_id"];
+
+    public static $rules =array(
+        'body'        =>  'required',
+        'user_id'     =>  'required|numeric',
+        'question_id' =>  'required|numeric',
+    );
+
+
+    public function post(){
+        return $this->belongsTo('App\Post');
+    }
+    public function user(){
+        return $this->belongsTo(\App\User::class,'user_id');
+    }
 }
