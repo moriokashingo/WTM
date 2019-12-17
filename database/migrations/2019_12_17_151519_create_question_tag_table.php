17ecd7d0a8fe6b757a1420c262945259aa12389d
moriokashingo <uma20056@yahoo.co.jp>
2019-12-18 01:08:43 +0900

    Create 2019_12_17_151519_create_question_tag_table.php

---
+++ database/migrations/2019_12_17_151519_create_question_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('question_id'); // 追加
+            $table->integer('tag_id'); // 追加
+            $table->softDeletes(); // 追加
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_tag');
+    }
+}
