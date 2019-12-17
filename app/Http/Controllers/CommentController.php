fe14adb017759f93e57d4dfbf6dd43ee6d09a62b
moriokashingo <uma20056@yahoo.co.jp>
2019-12-18 01:07:55 +0900

    Update CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
+use App\Question;
+use Auth;
 
 class CommentController extends Controller
 {
@@ -33,9 +36,12 @@ class CommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request, Question $question){
+        $comment= new Comment;
+        $input = $request->only($comment ->getFillable());
+        $comment  =$comment ->create($input);
+        return redirect()->action('QuestionController@show',$question);
+
     }
 
     /**
@@ -56,9 +62,15 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Question $question ,Comment $comment) 
     {
         //
+        $login_user_id=Auth::id();
+        if ($login_user_id == $question->user_id){
+            return view('comment.edit')->with(['question'=>$question, 'comment'=>$comment]);
+        } else {
+            return redirect()->action('QuestionController@show',$question);
+        }
     }
 
     /**
@@ -68,9 +80,15 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,$question_id,$comment_id) 
     {
         //
+
+        $comment= Comment::find($comment_id);
+        $form =$request->all();
+        unset($form['_token']);
+        $comment->fill($form)->save();
+        return redirect()->action('QuestionController@show', $question_id);
     }
 
     /**
@@ -79,8 +97,15 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question ,Comment $comment)
     {
-        //
+        $comment=Comment::find($comment->id);
+        $login_user_id=Auth::id();
+        if ($login_user_id == $comment->user_id){
+            Comment::find($comment->id)->delete();
+            return redirect()->action('QuestionController@show',$question);
+        } else {
+            return redirect()->action('CommentController@index');
+        }
     }
 }
