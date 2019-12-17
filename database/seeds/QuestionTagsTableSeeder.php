8293bde1b965a173b75c6b6c8ece5905cb962a08
moriokashingo <uma20056@yahoo.co.jp>
2019-12-18 01:08:35 +0900

    Create QuestionTagsTableSeeder.php

---
+++ database/seeds/QuestionTagsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuestionTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('question_tag')->insert([
+            ['question_id' => '1',
+            'tag_id' => '1',
+            ],
+            ['question_id' => '2',
+            'tag_id' => '1',
+            ],
+            ['question_id' => '2',
+            'tag_id' => '2',
+            ],
+            ['question_id' => '3',
+            'tag_id' => '3',
+            ],
+        ]);
+    }
+}
